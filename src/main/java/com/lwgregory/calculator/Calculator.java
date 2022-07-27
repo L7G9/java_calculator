39fa8af40b0bf0b215ec51db80601f3c0656e6d9
l7g9 <lukewgregory@gmail.com>
2022-07-27 21:39:57 +0000

    Add Javadoc comments

--- src/main/java/com/lwgregory/calculator/Calculator.java
+++ src/main/java/com/lwgregory/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package com.lwgregory.calculator;
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator buisiness logic
+ */
 @Service
 public class Calculator {
 	int sum(int a, int b) {
