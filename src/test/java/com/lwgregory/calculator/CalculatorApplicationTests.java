020d81e5a8ecff66aeb5d353456315ae0ac1478d
l7g9 <lukewgregory@gmail.com>
2022-07-27 08:48:27 +0000

    Fix so that test runs

--- src/test/java/com/lwgregory/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/lwgregory/calculator/CalculatorApplicationTests.java
@@ -2,12 +2,17 @@ package com.lwgregory.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private CalculatorController  controller;
 
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 }
--- src/test/java/com/lwgregory/calculator/CalculatorTest.java
+++ src/test/java/com/lwgregory/calculator/CalculatorTest.java
@@ -1,12 +1,16 @@
 package com.lwgregory.calculator;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class CalculatorTest {
+
   private Calculator calculator = new Calculator();
 
   @Test
-  public void testSum() {
+  public void testSum() throws Exception {
     assertEquals(5, calculator.sum(2, 3));
   }
 }
