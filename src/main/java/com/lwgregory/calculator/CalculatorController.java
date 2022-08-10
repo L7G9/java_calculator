88bbfb20c6ccf845cd7dde27a1c879891fc53204
l7g9 <lukewgregory@gmail.com>
2022-08-10 10:20:10 +0000

    Add CalculationRepository

--- src/main/java/com/lwgregory/calculator/CalculatorController.java
+++ src/main/java/com/lwgregory/calculator/CalculatorController.java
@@ -12,9 +12,14 @@ class CalculatorController {
 	@Autowired
 	private Calculator calculator;
 
+	@Autowired
+	private CalculationRepository calculationRepository;
+
 	@RequestMapping("/sum")
 	String sum(@RequestParam("a") Integer a,
 		   @RequestParam("b") Integer b) {
-		return String.valueOf(calculator.sum(a, b));
+		String result = String.valueOf(calculator.sum(a, b));
+		calculationRepository.save(new Calculation(a.toString(), b.toString(), result));
+		return result;
 	}
 }
