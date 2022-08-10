45bc215f02e8074ff1085cd6f0885036115894c6
l7g9 <lukewgregory@gmail.com>
2022-08-10 10:14:24 +0000

    Add CalculationReository class

---
+++ src/main/java/com/lwgregory/calculator/CalculationRepository.java
@@ -0,0 +1,5 @@
+package com.lwgregory.calculator;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CalculationRepository extends CrudRepository<Calculation,Integer> {}
+
