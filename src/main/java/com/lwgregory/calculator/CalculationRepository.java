bf5c698312c333ad586e078aaee092119577b7db
l7g9 <lukewgregory@gmail.com>
2022-08-10 13:09:54 +0000

    Add javadoc

--- src/main/java/com/lwgregory/calculator/CalculationRepository.java
+++ src/main/java/com/lwgregory/calculator/CalculationRepository.java
@@ -1,5 +1,8 @@
 package com.lwgregory.calculator;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+  CalculationRepository interface
+ */
 public interface CalculationRepository extends CrudRepository<Calculation,Integer> {}
 
